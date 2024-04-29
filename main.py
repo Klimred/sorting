ab343ca9abcb588fb2ff49327493d926ddc80180
Klimred <fedorov.klimentii@gmail.com>
2024-04-29 21:17:37 +0200

    cleaned up variable names as well as cleaner prints of results. Added the possibility to change the iteration amount as well as array size easily

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # This code is a simple implementation of the bubblesort and quicksort algorithms,
 # it also includes a simple GUI and the possibility to run the algorithms multiple times to get an average time.
 
-import sys
 from enum import Enum
 
 import numpy as np
@@ -30,10 +29,10 @@ class ArrayType(Enum):
     REVERSE_SORTED = 2
 
 
-array_mode = ArrayType.RANDOM
+array_mode = ArrayType.REVERSE_SORTED
 
 # Determines how much a sorted array is shuffled before being passed to the sorting algorithm
-degree_of_sortedness = 0.1
+degree_of_un_sortedness = 0.1
 
 
 # Function to generate a partially sorted array with n elements and a specified number of swaps
@@ -52,13 +51,15 @@ def generate_partially_sorted_array(n, num_swaps=3):
 def generate_random_array(n):
     if array_mode == ArrayType.PARTIALLY_SORTED:
         # Create a partially sorted array
-        sortable_arr = generate_partially_sorted_array(n, int(n*degree_of_sortedness))
+        sortable_arr = generate_partially_sorted_array(n, int(n * degree_of_un_sortedness))
     elif array_mode == ArrayType.RANDOM:
         # Create a 1D numpy array with a random permutation
         sortable_arr = np.random.permutation(n)
     elif array_mode == ArrayType.REVERSE_SORTED:
         # Create a reverse sorted array
         sortable_arr = list(range(n, 0, -1))
+    else:
+        raise ValueError("Invalid array mode")
     return sortable_arr
 
 # shows a plot of the array
@@ -85,7 +86,7 @@ def average_sort(sort_function):
     for i in range(iteration_amount):
         sort_function(generate_random_array(array_size))
     end = time.time()
-    print(f"{sort_function.__name__} took {end - start:.4f} seconds")
+    print(f"{iteration_amount} {sort_function.__name__}s took {end - start:.4f} seconds")
     print(f"  taking {(end - start) / iteration_amount:.4f} seconds on average")
 
 
@@ -96,11 +97,14 @@ root.geometry("300x300")
 root.title("Main Window")
 
 button_bubble = tk.Button(root, text="single bubblesort", command=lambda: sort_once(bubble_sort))
-button_bubble_average = tk.Button(root, text="100x bubblesort average", command=lambda: average_sort(bubble_sort))
+button_bubble_average = tk.Button(root, text=f"{iteration_amount} bubblesort average",
+                                  command=lambda: average_sort(bubble_sort))
 button_quick = tk.Button(root, text="single quicksort", command=lambda: sort_once(quicksort))
-button_bubble_quick = tk.Button(root, text="100x quicksort average", command=lambda: average_sort(quicksort))
+button_bubble_quick = tk.Button(root, text=f"{iteration_amount}x quicksort average",
+                                command=lambda: average_sort(quicksort))
 button_merge = tk.Button(root, text="single merge sort", command=lambda: sort_once(merge_sort))
-button_merge_average = tk.Button(root, text="100x merge sort average", command=lambda: average_sort(merge_sort))
+button_merge_average = tk.Button(root, text=f"{iteration_amount} merge sort average",
+                                 command=lambda: average_sort(merge_sort))
 button_quit = tk.Button(root, text="Quit", command=lambda: sys.exit())
 
 # arrange the buttons in a grid
@@ -114,5 +118,3 @@ button_quit.grid(row=3, column=0, columnspan=2, padx=100, pady=100)
 
 # start the main loop of the window
 tk.mainloop()
-
-
